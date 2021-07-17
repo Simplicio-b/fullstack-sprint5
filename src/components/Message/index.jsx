497643917a27211724f5cfe6faaefe7df653e0f2
Simplício Baiano <simpliciobaiano42@gmail.com>
2021-07-17 16:01:58 -0300

    styled components message

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,16 +1,20 @@
 import { useContext } from "react";
 import MessageContext from "../../contexts/MessageContext";
-import './Message.css';
+
+import {
+    Alert,
+    CloseBtn
+} from "./styles"
 
 function Message() {
     const { message, setMessage } = useContext(MessageContext);
 
     return (
         message &&
-        <div className="alert">
-            <span className="closebtn" onClick={() => setMessage("")} >&times;</span>
+        <Alert>
+            <CloseBtn onClick={() => setMessage("")} >&times;</CloseBtn>
             {message}
-        </div>
+        </Alert>
     );
 }
 
